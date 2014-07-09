698b00bfa9872ebd078b66685743ee89e72dadec
tsaltas <shannontsaltas@gmail.com>
2014-07-09 22:01:25 +0000

    checking secret

--- github_languages.py
+++ github_languages.py
@@ -4,10 +4,12 @@ from collections import defaultdict
 
 import requests
 
+from secret import USERNAME, PASSWORD
+
 def get_repositories(user):
     """ Retreive a list of a user's repositories """
     url = "https://api.github.com/users/{user}/repos".format(user=user)
-    response = requests.get(url)
+    response = requests.get(url, auth=(USERNAME, PASSWORD))
     return response.json()
 
 def main():
